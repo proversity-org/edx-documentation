3f117b3249af79d2ed0be1e8c26abd5244472dd2
Nico van Niekerk <nico.niekerk@gmail.com>
2017-09-19 09:19:32 +0200

    ENH: switching to circle 2.0

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ DEFAULT="deployer"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET=${DESTINATION_BUCKET}
 aws s3 sync $DIR s3://$BUCKET/ --delete --profile "$PROFILE"
-aws cloudfront create-invalidation --cli-input-json '{"DistributionId":"'"$AWS_CLOUDFRONT_ID"'","InvalidationBatch":{"Paths":{"Quantity":2,"Items":["/index.html","/*"]},"CallerReference": "'$(date "+%s")'"}}'
+aws cloudfront create-invalidation  --distribution-id $AWS_CLOUDFRONT_ID --cli-input-json '{"DistributionId":"'"$AWS_CLOUDFRONT_ID"'","InvalidationBatch":{"Paths":{"Quantity":2,"Items":["/index.html","/*"]},"CallerReference": "'$(date "+%s")'"}}'
 ERROR=$?
 if [ "$ERROR" -eq 0 ];
 then
